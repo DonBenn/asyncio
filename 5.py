578de4c12f811c212a56a5a6c29e447d2403711d
Denis Bessonov <denibessono@yandex.ru>
2025-05-29 14:07:10 +0300

    Tasks

--- 5.py
+++ 5.py
@@ -17,3 +17,50 @@ async def main():
     await asyncio.gather(*tasks)
 
 asyncio.run(main())
+
+
+
+import asyncio
+
+async def print_with_delay(num):
+    print(f'Coroutine {num} is done')
+    await asyncio.sleep(1)
+
+async def main():
+    tasks = []
+    for num in range(10):
+        task = asyncio.create_task(print_with_delay(num))  # создаем 10 задач
+        tasks.append(task)  # добавляем все задачи в список tasks
+    await asyncio.gather(*tasks)  # запускаем все задачи из списка tasks
+
+asyncio.run(main())
+
+
+
+import asyncio
+
+
+counters = {
+    "Counter 1": 0,
+    "Counter 2": 0
+}
+
+max_counts = {
+    "Counter 1": 13,
+    "Counter 2": 7
+}
+
+async def counter(counter_name, sec):
+    while counters[counter_name] < max_counts[counter_name]:
+        counters[counter_name] += sec
+        await asyncio.sleep(sec)
+        print(f'{counter_name}: {counters[counter_name]}')
+
+
+async def main():
+    task1 = asyncio.create_task(counter("Counter 1", 1))
+    task2 = asyncio.create_task(counter("Counter 2", 1))
+    await task1
+    await task2
+
+asyncio.run(main())
