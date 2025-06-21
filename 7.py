f1c3fd659e64eb9ed3f82575deaaef40e9c16d35
Denis Bessonov <denibessono@yandex.ru>
2025-06-21 12:22:49 +0300

    Awaitable objects

--- 7.py
+++ 7.py
@@ -43,3 +43,50 @@ async def main():
 start = time.time()
 asyncio.run(main())
 print(f'Программа выполнена за {time.time()-start:.3f} секунд(ы)')
+
+
+
+import asyncio
+
+async def read_book(student, time):
+    print(f"{student} начал читать книгу.")
+    await asyncio.sleep(time)
+    print(f"{student} закончил читать книгу за {time} секунд.")
+
+
+async def main():
+    task1 = asyncio.create_task(read_book("Алекс", 5))
+    task2 = asyncio.create_task(read_book("Мария", 3))
+    task3 = asyncio.create_task(read_book("Иван", 4))
+    await asyncio.gather(task1, task2, task3)
+
+asyncio.run(main())
+
+
+
+import asyncio
+
+students = {
+    "Алекс": {"course": "Асинхронный Python", "steps": 515, "speed": 78},
+    "Мария": {"course": "Многопоточный Python", "steps": 431, "speed": 62},
+    "Иван": {"course": "WEB Парсинг на Python", "steps": 491, "speed": 57}
+}
+
+async def study_course(student, course, steps, speed):
+    print(f'{student} начал проходить курс {course}.')
+    result = round(steps / speed, 2)
+    await asyncio.sleep(result)
+    print(f'{student} прошел курс {course} за {result} ч.')
+
+
+async def main():
+    student_items = [
+        asyncio.create_task(study_course(
+            student, value['course'], value['steps'], value['speed']))
+        for student, value in students.items()
+    ]
+
+    await asyncio.gather(*student_items)
+
+asyncio.run(main())
+
