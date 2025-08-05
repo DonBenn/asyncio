a7bd8b16d14b389151e45da2bc1ae9e09bf81bd1
Denis Bessonov <denibessono@yandex.ru>
2025-08-05 11:50:11 +0300

    Run asyncio.run() continue

--- 9.py
+++ 9.py
@@ -92,3 +92,265 @@ async def main():
     await asyncio.gather(*tasks)
 
 asyncio.run(main())
+
+
+
+import asyncio
+
+async def coroutine_1():
+    print("Первое сообщение от корутины 1")
+    await asyncio.sleep(2)  # Подберите необходимую задержку
+    print("Второе сообщение от корутины 1")
+
+async def coroutine_2():
+    print("Первое сообщение от корутины 2")
+    await asyncio.sleep(1.5)  # Подберите необходимую задержку
+    print("Второе сообщение от корутины 2")
+
+async def coroutine_3():
+    print("Первое сообщение от корутины 3")
+    await asyncio.sleep(1)  # Подберите необходимую задержку
+    print("Второе сообщение от корутины 3")
+
+async def main():
+    await asyncio.gather(
+        coroutine_1(),
+        coroutine_2(),
+        coroutine_3(),
+    )
+
+asyncio.run(main())
+
+
+
+import asyncio
+
+async def coroutine_1(delay=0.1):
+    print("Первое сообщение от корутины 1")
+    await asyncio.sleep(0.4)  # Первая задержка
+
+    print("Второе сообщение от корутины 1")
+    await asyncio.sleep(0.7)  # Вторая задержка
+
+    print("Третье сообщение от корутины 1")
+    await asyncio.sleep(0.1)  # Третья задержка
+
+    print("Четвертое сообщение от корутины 1")
+
+
+async def coroutine_2(delay=0.1):
+    print("Первое сообщение от корутины 2")
+    await asyncio.sleep(0.3)  # Первая задержка
+
+    print("Второе сообщение от корутины 2")
+    await asyncio.sleep(0.4)  # Вторая задержка
+
+    print("Третье сообщение от корутины 2")
+    await asyncio.sleep(0.95)  # Третья задержка
+
+    print("Четвертое сообщение от корутины 2")
+
+
+async def coroutine_3(delay=0.1):
+    print("Первое сообщение от корутины 3")
+    await asyncio.sleep(0.2)  # Первая задержка
+
+    print("Второе сообщение от корутины 3")
+    await asyncio.sleep(0.6)  # Вторая задержка
+
+    print("Третье сообщение от корутины 3")
+    await asyncio.sleep(0.8)  # Третья задержка
+
+    print("Четвертое сообщение от корутины 3")
+
+async def main():
+    await asyncio.gather(
+        coroutine_1(),
+        coroutine_2(),
+        coroutine_3(),
+    )
+
+asyncio.run(main())
+
+
+
+import asyncio
+
+async def coroutine_1(delay=0.1):
+    print("Первое сообщение от корутины 1")
+    await asyncio.sleep(0.3)  # Первая задержка
+
+    print("Второе сообщение от корутины 1")
+    await asyncio.sleep(0.3)  # Вторая задержка
+
+    print("Третье сообщение от корутины 1")
+    await asyncio.sleep(0.1)  # Третья задержка
+
+    print("Четвертое сообщение от корутины 1")
+
+
+async def coroutine_2(delay=0.1):
+    print("Первое сообщение от корутины 2")
+    await asyncio.sleep(0.25)  # Первая задержка
+
+    print("Второе сообщение от корутины 2")
+    await asyncio.sleep(0.3)  # Вторая задержка
+
+    print("Третье сообщение от корутины 2")
+    await asyncio.sleep(0.3)  # Третья задержка
+
+    print("Четвертое сообщение от корутины 2")
+
+
+async def coroutine_3(delay=0.1):
+    print("Первое сообщение от корутины 3")
+    await asyncio.sleep(0.2)  # Первая задержка
+
+    print("Второе сообщение от корутины 3")
+    await asyncio.sleep(0.4)  # Вторая задержка
+
+    print("Третье сообщение от корутины 3")
+    await asyncio.sleep(0.2)  # Третья задержка
+
+    print("Четвертое сообщение от корутины 3")
+
+async def main():
+    await asyncio.gather(
+        coroutine_1(),
+        coroutine_2(),
+        coroutine_3(),
+    )
+
+asyncio.run(main())
+
+
+
+import asyncio
+
+async def coroutine_1():
+    await asyncio.sleep(1.1)  # Задержка для первого сообщения
+    print("Сообщение 1 от корутины 1")
+    await asyncio.sleep(1.1)  # Задержка для второго сообщения
+    print("Сообщение 2 от корутины 1")
+
+async def coroutine_2():
+    await asyncio.sleep(1.2)
+    print("Сообщение 1 от корутины 2")
+    await asyncio.sleep(1.8)
+    print("Сообщение 2 от корутины 2")
+
+async def coroutine_3():
+    await asyncio.sleep(1.3)
+    print("Сообщение 1 от корутины 3")
+    await asyncio.sleep(1.9)
+    print("Сообщение 2 от корутины 3")
+
+async def coroutine_4():
+    await asyncio.sleep(1.4)
+    print("Сообщение 1 от корутины 4")
+    await asyncio.sleep(2.0)
+    print("Сообщение 2 от корутины 4")
+
+async def coroutine_5():
+    await asyncio.sleep(1.5)
+    print("Сообщение 1 от корутины 5")
+    await asyncio.sleep(2.1)
+    print("Сообщение 2 от корутины 5")
+
+async def coroutine_6():
+    await asyncio.sleep(1.6)
+    print("Сообщение 1 от корутины 6")
+    await asyncio.sleep(2.2)
+    print("Сообщение 2 от корутины 6")
+
+async def main():
+    await asyncio.gather(
+        coroutine_1(),
+        coroutine_2(),
+        coroutine_3(),
+        coroutine_4(),
+        coroutine_5(),
+        coroutine_6(),
+    )
+
+asyncio.run(main())
+
+
+
+import asyncio
+import random
+
+random.seed(1)
+
+class MailServer:
+    def __init__(self):
+        self.mailbox = ["Привет!", "Встреча в 15:00", "Важное уведомление", "Реклама"]
+
+    async def check_for_new_mail(self):
+        if random.random() < 0.1:
+            return "Ошибка при проверке новых писем."
+        return random.choice([True, False])
+
+    async def fetch_new_mail(self):
+        mail = random.choice(self.mailbox)
+        return f"Новое письмо: {mail}"
+
+
+async def check_mail(server):
+
+    while True:
+        await asyncio.sleep(1)
+        check = await server.check_for_new_mail()
+        if check == "Ошибка при проверке новых писем.":
+            print("Ошибка при проверке новых писем.")
+            break
+        if check:
+            print(await server.fetch_new_mail())
+        else:
+            print("Новых писем нет.")
+
+
+async def main():
+    await check_mail(MailServer())
+
+
+asyncio.run(main())
+
+
+
+import asyncio
+import time
+
+
+async def process_request(request_name, stages, status):
+    for stage_name in stages:
+        # await asyncio.sleep(0)
+        time.sleep(1)  # Симулируем время выполнения этапа
+        status[request_name] = stage_name
+        await asyncio.sleep(0)
+
+
+async def update_status(request_name, status):
+    while True:
+        # await asyncio.sleep(0)
+        print(status)
+        if status == {request_name: 'Отправка уведомлений'}:
+            break
+        await asyncio.sleep(0)
+
+
+async def main():
+    # Исходные данные по запросу и этапам его обработки
+    request_name = 'Запрос 1'
+    stages = ["Загрузка данных", "Проверка данных", "Анализ данных", "Сохранение результатов", "Отправка уведомлений"]
+
+    status = {request_name: None}
+
+    # Создание задач для каждой корутины
+    process_task = asyncio.create_task(process_request(request_name, stages, status))
+    updater_task = asyncio.create_task(update_status(request_name, status))
+
+    await asyncio.gather(process_task, updater_task)
+
+
+asyncio.run(main())
